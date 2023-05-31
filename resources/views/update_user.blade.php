74c7527d7c3d17f96509e0f3bb189f1df764e3b4
ChuDuyHung <duyhungsdbg@gmail.com>
2023-05-31 14:15:29 +0700

    add bootstrap

--- resources/views/update_user.blade.php
+++ resources/views/update_user.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>UpdateUser</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <title>Update</title>
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" />
 </head>
 
 <body>
@@ -26,7 +27,7 @@
         </style>
         <h2>Cap Nhat</h2>
         <table>
-            <form action="" method="post">
+            <form action="" method="post" class="mb-3">
                 @if($errors->any())
                 @foreach($errors->all() as $errors)
                 <p>{{$errors}}</p>
@@ -44,11 +45,14 @@
                     <td><input name="email" type="text" value="{{$infomation->email}}"></td>
                     <td><input name="sdt" type="text" value="{{$infomation->sdt}}"></td>
                 </tr>
-                <h2><input type="submit" value="Cap Nhat"></h2>
+                <div class="col-2">
+                    <button type="submit" class="btn btn-primary btn-block">Cap Nhat</button>
+                </div>
                 </from>
         </table>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
 </body>
+
 </html>
